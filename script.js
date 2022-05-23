f1bff0423a6b094d81edffa5c809e1095fe15295
TTS <90527370+TallerThanShort@users.noreply.github.com>
2022-05-23 19:10:35 +0200

    Small Things are O K

--- script.js
+++ script.js
@@ -19,13 +19,11 @@ window.onload = () => {
 
         localStorage.setItem('oauth-state', randomString);
     }
-
-		
     
     if(usertime != null){
         document.getElementById('navbarthings').innerHTML += `<span class="link-1naFFL link-2eyudH">${loggedAs}</span>`;
         console.log(`${localStorage.getItem('username')}#${localStorage.getItem('discriminator')}`)
     } else{
-        document.getElementById('navbarthings').innerHTML += `<span class="link-1naFFL link-2eyudH" onclick="document.location.href = 'https://discord.com/api/oauth2/authorize?client_id=914396590901452820&redirect_uri=http%3A%2F%2Fdemo.bottombots.xyz%2Fauth&response_type=token&scope=identify%20email&state=${localStorage.getItem('randomString')}';">Log In</span>`
+        document.getElementById('navbarthings').innerHTML += `<span class="link-1naFFL link-2eyudH" onclick="document.location.href = 'https://discord.com/api/oauth2/authorize?client_id=914396590901452820&redirect_uri=http%3A%2F%2Fdemo.bottombots.xyz%2Fauth&response_type=token&scope=identify%20email&state=${localStorage.getItem('oauth-state')}';">Log In</span>`
     }
 };
